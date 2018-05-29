d5240ad9df16d501f69e44cd98a998c8216a22db
Wesley Jaemers <jaemers.wesley@gmail.com>
2018-05-29 13:43:21 +0200

    Java packages

--- VdabDemo/src/Object.java
+++ VdabDemo/src/Object.java
@@ -3,11 +3,22 @@ import java.util.Scanner;
 public class Object {
     public static void main(String[] args) {
 
-        String naam = "Joskeuh";
+        String naam = "Joskeuh Vermeulen";
         String naam2 = new String ("Joskeuh");
 
         Scanner scanner = new Scanner(System.in);
 
-        
+        System.out.print("Geef een getal op:");
+        int j = scanner.nextInt();
+        System.out.println("res= "+ j*2);
+
+        System.out.println(naam.toUpperCase());
+
+        //Substring
+        int i = naam.indexOf(' ');
+         String firstName = naam.substring(0, i);
+
+         //
+
     }
 }
