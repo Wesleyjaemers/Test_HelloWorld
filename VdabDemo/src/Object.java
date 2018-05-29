25531d72cbc83b8cd154ad72431f8b170e523635
Wesley Jaemers <jaemers.wesley@gmail.com>
2018-05-29 12:02:07 +0200

    Toevoeging Object

---
+++ VdabDemo/src/Object.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Object {
+    public static void main(String[] args) {
+
+        String naam = "Joskeuh";
+        String naam2 = new String ("Joskeuh");
+
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}
---
+++ VdabDemo/src/Salaris.java
@@ -0,0 +1,10 @@
+public class Salaris {
+    public static void main(String[] args) {
+
+        double salary = 2500;
+
+        double rsz = salary *(1-0.1307);
+        System.out.println(rsz);
+
+    }
+}
