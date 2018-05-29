c1e603c5d83441895b0046f3573f9e865b404b7a
Wesley Jaemers <jaemers.wesley@gmail.com>
2018-05-29 13:44:23 +0200

    Fibonacci update

--- HelloWorld/src/Fibonacci.java
+++ HelloWorld/src/Fibonacci.java
@@ -1,5 +1,15 @@
 public class Fibonacci {
     public static void main(String[] args) {
-        
+        int x = 0;
+        int y = 1;
+
+        for (int i = 1; i<=20;i++){
+
+            x=x+y;
+            y=x+y;
+            System.out.print(x + " " + y + " ");
+
+        }
+
     }
 }
