21a9126f9a60103fbf13064dac7dc1f86314321b
Wesley Jaemers <jaemers.wesley@gmail.com>
2018-05-29 15:51:40 +0200

    Fibonacci en NumberPyramid

---
+++ VdabDemo/src/java_packages/Fibonacci.java
@@ -0,0 +1,22 @@
+package java_packages;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+
+
+        int n = 20;
+        int a = 0;
+        int b = 1;
+
+        System.out.print(a+" ");
+        System.out.print(b+" ");
+
+        for (int i = 1; i <= n; i++)
+            {
+                int som = a + b;
+                System.out.print(som + " ");
+                a = b;
+                b = som;
+        }
+    }
+}
---
+++ VdabDemo/src/java_packages/NumberPyramid.java
@@ -0,0 +1,24 @@
+package java_packages;
+
+public class NumberPyramid {
+    public static void main(String[] args) {
+
+        int i = 0;
+        int max = 7;
+
+        while(i <= max){
+
+
+            //count powers up
+            for(int j=0; j <= i; j++){
+                System.out.print((int)Math.pow(2, j)+ " ");
+            }
+            //count powers down
+            for (int k= i-1; k >= 0;k--){
+                System.out.print((int)Math.pow(2, k)+ " ");
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
