d3b74c3a3d1467ce8d783f7816c223d33b8d7617
Wesley <jaemers.wesley@gmail.com>
2018-05-31 01:09:07 +0200

    Update Faculteit

---
+++ VdabDemo/src/java_packages/Faculteit.java
@@ -0,0 +1,21 @@
+package java_packages;
+
+import java.util.Scanner;
+
+public class Faculteit {
+    public static void main(String[] args) {
+
+        double res;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Geef getal op:");
+        int fac = input.nextInt();
+        res=fac;
+
+        for (int i=1;i<fac;i++){
+            res = res*(fac-i);
+        }
+        System.out.println("Faculteit van "+fac+" is: "+res);
+    }
+}
