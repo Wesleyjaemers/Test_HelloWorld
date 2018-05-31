5e444dba8de58f42bd8d46f2f48d49d17cf4b80d
Wesley Jaemers <jaemers.wesley@gmail.com>
2018-05-31 09:55:45 +0200

    Update Prime

--- VdabDemo/src/java_packages/Prime.java
+++ VdabDemo/src/java_packages/Prime.java
@@ -5,24 +5,25 @@ import java.util.Scanner;
 public class Prime {
     public static void main(String[] args) {
 
-        double input;
-        double round;
-        double rest;
-         Scanner Prime = new Scanner(System.in);
+        int rest;
+        Scanner Prime = new Scanner(System.in);
+        Boolean isPrime = true;
 
         System.out.println("Geef getal op: ");
         int a = Prime.nextInt();
-        a = (int)Math.sqrt(a);
 
-        for (int b=2;b<a;b++){
-            input = a / b;
-            round = Math.round(input);
-            rest = input - round;
-           if (rest==0){
-               System.out.println("Getal is geen priemgetal");
-           }
-            System.out.println("Getal is een priemgetal");
+        for (int i=2;i<a;i++){
+
+            rest = a%i;
+            if (rest==0) {
+                isPrime = false;}
         }
+
+        if (isPrime==false)
+            System.out.println("getal is geen priem");
+            
+        else
+            System.out.println("getal is een priem");
 
     }
 }
