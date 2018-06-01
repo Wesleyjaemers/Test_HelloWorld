486069a7663b005be9e9e03e708fd2692d672f30
Wesley Jaemers <jaemers.wesley@gmail.com>
2018-06-01 09:08:13 +0200

    Arrays + ArrayOef

---
+++ VdabDemo/src/java_packages/ArrayOef.java
@@ -0,0 +1,13 @@
+package java_packages;
+
+public class ArrayOef {
+    public static void main(String[] args) {
+
+        int [] array = {1, 38, 5, 15, -48 ,4, 76};
+        int length = array.length;
+
+        for (int i=0;i<length;i++){
+            System.out.println(array[i]);
+        }
+    }
+}
---
+++ VdabDemo/src/java_packages/Arrays.java
@@ -0,0 +1,18 @@
+package java_packages;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+
+        int i =5;  //Scalar variable
+
+        int [] j = {1, 2, 5, 7, 9, -6, 12};
+        System.out.println(j.length);   // print lengte van de array, 7 vakjes in dit geval
+
+        int index = 1+1;
+        int getal = j [2];
+        System.out.println(index); // waarde opvragen op index 2
+
+
+    }
+}
