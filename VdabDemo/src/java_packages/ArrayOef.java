6b8962bd77fad7d2634acc6744f19b28913f3572
Wesley Jaemers <jaemers.wesley@gmail.com>
2018-06-01 11:43:31 +0200

    Update Arrays + ArrayOef

--- VdabDemo/src/java_packages/ArrayOef.java
+++ VdabDemo/src/java_packages/ArrayOef.java
@@ -3,11 +3,31 @@ package java_packages;
 public class ArrayOef {
     public static void main(String[] args) {
 
-        int [] array = {1, 38, 5, 15, -48 ,4, 76};
+        int[] array = {1, 38, 5, 15, -48, 4, 76};
         int length = array.length;
 
-        for (int i=0;i<length;i++){
+        for (int i = 0; i < length; i++) {
             System.out.println(array[i]);
         }
+
+        for (int i = 0; i < array.length; i++) {
+            int temp = array[i];
+            int doub = temp*2;
+            array[i] = doub;    // Hiermee kunnen we de lijst herschrijven
+
+        }
+
+        String[] arrayNaam = {"Ludo", "Erik", "Johan", "Patrick", "Joske"}; // print de volledige lijst maar parameters kunnen aangepast worden
+        for (int i = 0; i < arrayNaam.length; i++) {
+            System.out.println(arrayNaam[i]);
+        }
+
+        for (String naam: arrayNaam){ // print de volledige lijst
+            stuurEmailNaar(naam);
+        }
+
+    }
+    public static void stuurEmailNaar (String email){
+        //....
     }
 }
--- VdabDemo/src/java_packages/Arrays.java
+++ VdabDemo/src/java_packages/Arrays.java
@@ -4,15 +4,32 @@ public class Arrays {
 
     public static void main(String[] args) {
 
-        int i =5;  //Scalar variable
+        int i = 5;  //Scalar variable
 
-        int [] j = {1, 2, 5, 7, 9, -6, 12};
+        int[] j = {1, 2, 5, 7, 9, -6, 12};
         System.out.println(j.length);   // print lengte van de array, 7 vakjes in dit geval
 
-        int index = 1+1;
-        int getal = j [2];
+        int index = 1 + 1;
+        int getal = j[2];
         System.out.println(index); // waarde opvragen op index 2
 
+        int[] getallen = new int[20]; //nieuwe array met default waarden (in dit geval 0), 20 posities
+        for (int k : getallen) {
+
+            System.out.println(k);
+        }
+        String[] getallen2 = new String[20]; // Als het geen primitief datatype is gaat de waarde 'null' zijn, 20 posities
+        for (String k : getallen2) {
+
+            System.out.println(k);
+        }
+
+        double x = 5;
+        double y = 0;
+        double res;
+        res = x/y;
+        System.out.println(res); // dit geeft resultaat INFINITY
+        System.out.println(res/res); // dit geeft NaN (Not a Number)
 
     }
 }
